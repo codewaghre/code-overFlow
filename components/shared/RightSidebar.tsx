bff6a38bb5008c11a12ed066de6f75c6bf72bc58
Waghre Abhishek <waghreabhishek@gmail.com>
2025-04-19 00:09:02 +0530

    show right side top question and tags

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,27 +1,19 @@
 import React from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
-
 import RenderTag from './RenderTag';
 
+import { getTopPopularTags } from '@/lib/actions/tag.action';
+import { getHotQuestions } from '@/lib/actions/question.action';
 
-const RightSidebar = () => {
 
-    const hotQuestions = [
-        { _id: 1, title: "How do i use Exprss as coustom Server ins NEXTJS" },
-        { _id: 2, title: "How do i use Exprss as coustom Server ins NEXTJS" },
-        { _id: 3, title: "How do i use Exprss as coustom Server ins NEXTJS" },
-        { _id: 4, title: "How do i use Exprss as coustom Server ins NEXTJS" }
-    ];
+const RightSidebar = async() => {
 
-    const popularTags = [
-        { _id: 1, name: "javascript", numberOfQuestions: 8 },
-        { _id: 2, name: "javascript", numberOfQuestions: 8 },
-        { _id: 3, name: "javascript", numberOfQuestions: 8 },
-        {_id: 4, name: "javascript" , numberOfQuestions: 8}
-    ]
-    return (
-       <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getTopPopularTags();
+
+  return (
+    <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
         <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
         <div className="mt-7 flex w-full flex-col gap-[30px]">
@@ -32,7 +24,7 @@ const RightSidebar = () => {
               className='flex cursor-pointer items-center justify-between gap-7'
             >
               <p className="body-medium text-dark500_light700">{question.title}</p>
-              <Image 
+              <Image
                 src="/assets/icons/chevron-right.svg"
                 alt="chevron right"
                 width={20}
@@ -44,17 +36,17 @@ const RightSidebar = () => {
         </div>
       </div>
       <div className="mt-16">
-      <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+        <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
         <div className="mt-7 flex flex-col gap-4">
-            {popularTags.map((tag) => (
-              <RenderTag 
-                key={tag._id}
-                _id={tag._id}
-                name={tag.name}
-                totalQuestions={tag.numberOfQuestions}
-                showCount
-              />
-            ))}
+          {popularTags.map((tag) => (
+            <RenderTag
+              key={tag._id}
+              _id={tag._id}
+              name={tag.name}
+              totalQuestions={tag.numberOfQuestions}
+              showCount
+            />
+          ))}
         </div>
       </div>
     </section>
@@ -62,3 +54,5 @@ const RightSidebar = () => {
 }
 
 export default RightSidebar
+
+
