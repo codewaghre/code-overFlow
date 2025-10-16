572499601d36b9aeb7181b545edc4d1b18726952
Waghre Abhishek <waghreabhishek@gmail.com>
2025-10-16 16:56:58 +0530

    ask question

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -2,23 +2,26 @@ import Question from '@/components/form/Question'
 import { getUserByID } from '@/lib/actions/users.action'
 import { auth } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
-import React from 'react'
+import React, { Suspense } from 'react'
 
-const page = async() => {
+const page = async () => {
 
   const session = await auth();
   const { userId } = session;
-  
+
   if (!userId) redirect("/sign-in")
-  
+
   const mongoUser = await getUserByID({ userId })
-    
+
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
 
       <div className="mt-9">
-        <Question mongoUserId={JSON.stringify(mongoUser?._id)} />
+        <Suspense fallback={" "}>
+          <Question mongoUserId={JSON.stringify(mongoUser?._id)} />
+        </Suspense>
+        {/* <Question mongoUserId={JSON.stringify(mongoUser?._id)} /> */}
       </div>
     </div>
   )
